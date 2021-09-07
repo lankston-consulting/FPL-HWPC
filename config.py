a9cf4b210587536d9113ed060269949d94322a61
colto <colton11gerth@gmail.com>
2021-09-07 15:16:06 -0500

    config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from utils import user_data_helper
+"""Credentials for RPMS App."""
+
+
+# The private key associated with your service account in JSON format.
+GCP_PRIVATE_KEY_FILE = 'hwpc-sa.json'
+
+###############################################################################
+#                               Initialization.                               #
+###############################################################################
+gch = user_data_helper.UserData(use_service_account={'keyfile': GCP_PRIVATE_KEY_FILE})
