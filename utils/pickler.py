282ad7dbb8efe53876b3581f0eaf11758bcbb723
Robb Lankston <robblankston@gmail.com>
2021-09-22 15:19:29 -0700

    Expanded pickling to a class
    This will allow any class to use the basic pickling functions without duplicating code.

---
+++ utils/pickler.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+import pickle
+
+class Pickler(ABC):
+
+    def pickle(self) -> None:
+        """Basic function to pickle an object and give it the class name
+        """
+        n = self.__class__.__name__
+        with open(n + '.pkl', 'wb') as p:
+            pickle.dump(self, p)
+
+    @classmethod
+    def unpickle(cls):
+        """Basic function to load pickled object from disk
+
+        Returns:
+            [type]: [description]
+        """
+        n = cls.__name__
+        with open(n + '.pkl', 'rb') as p:
+            return pickle.load(p)
