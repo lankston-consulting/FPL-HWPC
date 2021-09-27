0cf178ce44c482386f7270b8a23ed731768c9c2d
Robb Lankston <robblankston@gmail.com>
2021-09-27 14:28:27 -0700

    Dynamic input processing
    The argparser is setup now to take a command line argument for the uploaded input. I also cleaned up input_download.py to conform to the style guide and not rely on ambiguous scope.

--- hwpc/input_download.py
+++ hwpc/input_download.py
@@ -8,36 +8,41 @@ class InputDownload(object):
     def __init__(self) -> None:
         super().__init__()
 
-    def downloads(self):
-        test = gch.download_file('hwpcarbon-data','hpwc-user-inputs/user_request_20210927_193455/user_input.json')
+    def downloads(self, path):
+        if not os.path.exists('data'):
+            os.makedirs('data')
 
-        with open("utils/default_paths.json", "r") as readjson:
-            default_json = json.load(readjson)
-        filled = test.read()
-        filled = json.loads(filled)
-        test.close()
-        inputs = {}
+        with (gch.download_file('hwpcarbon-data', path)) as online_data:
 
-        for default_key,default_value in default_json.items():
-            if(default_key in filled):
-                data_path = "data/"+default_key+".csv"
-                file_path = filled[default_key]
-                gch.download_blob('hwpcarbon-data',file_path,data_path)
-                inputs[default_key] = data_path
-            else:
-                inputs[default_key] = "data/"+default_key+".csv"
-                shutil.copy(default_value, 'data')
+            with open("utils/default_paths.json", "r") as readjson:
+                default_json = json.load(readjson)
+            
+                filled = online_data.read()
+                filled = json.loads(filled)
+                
+                inputs = {}
 
-        for filled_key,filled_value in filled.items():
-            if(filled_key not in inputs):
-                data_path = "data/"+filled_key
-                file_path = filled[filled_key]
-                inputs[filled_key] = data_path
-                gch.download_blob('hwpcarbon-data',file_path,data_path)
+                for default_key, default_value in default_json.items():
+                    if(default_key in filled):
+                        data_path = "data/" + default_key + ".csv"
+                        file_path = filled[default_key]
+                        gch.download_blob('hwpcarbon-data', file_path,data_path)
+                        inputs[default_key] = data_path
+                    else:
+                        inputs[default_key] = "data/" + default_key + ".csv"
+                        shutil.copy(default_value, 'data')
 
-        with open('data/inputs.json', 'w') as outfile:
-            json.dump(inputs, outfile)
+                for filled_key,filled_value in filled.items():
+                    if filled_key not in inputs:
+                        data_path = "data/" + filled_key
+                        file_path = filled[filled_key]
+                        inputs[filled_key] = data_path
+                        gch.download_blob('hwpcarbon-data', file_path,data_path)
 
-        for key,value in inputs.items():
-            print(key,value)
+                with open('data/inputs.json', 'w') as outfile:
+                    json.dump(inputs, outfile)
+
+                for key, value in inputs.items():
+                    print(key, value)
+
         print('done')
