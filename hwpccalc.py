80a6b052a86f87c0bc06800b3f2d767129cf8229
Robb Lankston <robblankston@gmail.com>
2021-11-17 11:54:24 -0800

    New duplicate fix to try.

--- hwpccalc.py
+++ hwpccalc.py
@@ -8,8 +8,7 @@ from hwpc import names
 from hwpc import results
 
 def run(path='hpwc-user-inputs/83d4b4ef-d57a-4995-8342-489ca4d769b3', name='test'):
-    # 
-    #hpwc-user-inputs/user_request_20210929_130704
+
     path = path
     names.Names()
     names.Names.Tables()
@@ -26,28 +25,9 @@ def run(path='hpwc-user-inputs/83d4b4ef-d57a-4995-8342-489ca4d769b3', name='test
 
     m.run()
 
-    # m.md.pickle()
-    # m.results.pickle()
-
-    # md = model_data.ModelData.unpickle()
-
-    # r = results.Results.unpickle()
-    # r.total_yearly_harvest()
-
     print('model finished.')
 
     return
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    
-    # parser.add_argument(
-    #     '-p', 
-    #     help='Path to the uploaded user data.', 
-    #     default='hpwc-user-inputs/user_request_20210929_130704',
-    # )
-    
-    # args, _ = parser.parse_known_args()
-
-    # run(args)
     run()
