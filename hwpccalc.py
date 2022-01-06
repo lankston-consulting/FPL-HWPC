66124b4a2e056581fd06f2f7e4c17d8e72cd83da
colto <colton11gerth@gmail.com>
2022-01-06 14:13:41 -0600

    sendgrid email update, still needs api key and testing

--- hwpccalc.py
+++ hwpccalc.py
@@ -6,6 +6,7 @@ from hwpc import model_data
 from hwpc import input_download
 from hwpc import names
 from hwpc import results
+from hwpc import email
 
 def run(path='hpwc-user-inputs/3097a3de-5fc0-4f16-988f-cd739e08ac4e', name='robbdemo3'):
 
@@ -21,10 +22,16 @@ def run(path='hpwc-user-inputs/3097a3de-5fc0-4f16-988f-cd739e08ac4e', name='robb
     i = input_download.InputDownload()
     i.downloads()
 
+   
+
+
+
     m = model.Model()
 
     m.run()
 
+    e = email.Email()
+    e.send_email(m.md.data['email'])
     print('model finished.')
 
     return
