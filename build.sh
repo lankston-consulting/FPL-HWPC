5c897047c618d5bcba730742599643dd7e15ef0b
Robb Lankston <robblankston@gmail.com>
2022-10-26 14:10:32 -0700

    Possible client container fix

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
 echo Building hwpc-calc
-pipenv run pipenv-setup sync
-pipenv run python -m build
-pipenv requirements > requirements.txt
+# pipenv run pipenv-setup sync
+# pipenv run python -m build
+# pipenv requirements > requirements.txt
 docker build -t hwpc-calc:latest . 
 docker tag hwpc-calc:latest 234659567514.dkr.ecr.us-west-2.amazonaws.com/hwpc-calc:latest
 # docker push 234659567514.dkr.ecr.us-west-2.amazonaws.com/hwpc-calc:latest
