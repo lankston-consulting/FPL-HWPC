f06d33b90dce32c75de625f575dcfde08aae1b8a
Robb Lankston <robb@lankstonconsulting.com>
2022-12-16 11:59:25 -1000

    Added json and other configuration files needed for deployment

---
+++ src/arch/lambda-get-user-input.py
@@ -0,0 +1,91 @@
+import json
+import urllib.parse
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+def runCalculatorTask(name, user_string):
+    print(name)
+    print(user_string)
+    #run a ECS fargate task
+    client = boto3.client('ecs')
+    response = client.run_task(
+        cluster = "Calc-Fargate-Cluster",
+        launchType = "FARGATE",
+        taskDefinition = "calc-fargate-task",
+        count = 1,
+        platformVersion = "LATEST",
+        networkConfiguration={
+        'awsvpcConfiguration': {
+            'subnets': [
+                'subnet-0a67a553e8d4a6e46',
+            ],
+            'securityGroups': [
+                'sg-013bca134dc371041',
+            ],
+            'assignPublicIp': 'ENABLED'
+        }
+    },
+        overrides = {
+            'containerOverrides': [
+                {
+                    "name": "hwpc-calc",
+                    'command': [
+                    "-p", user_string, "-n", name,
+                    ],
+                    
+                    "environment": [
+                        {
+                            "name": "AWS_ACCESS_KEY_ID",
+                            "value": "X"
+                        },
+                        {
+                            "name": "AWS_SECRET_ACCESS_KEY",
+                            "value": "X"
+                        },
+                        {
+                            "name": "S3_INPUT_BUCKET",
+                            "value": "hwpc"
+                        },
+                        {
+                            "name": "S3_OUTPUT_BUCKET",
+                            "value": "hwpc_output"
+                        },
+                    
+                    
+                        ]
+                }
+                ]
+        }
+        )
+    
+    
+    
+def lambda_handler(event, context):
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        # print("CONTENT TYPE: " + response['ContentType'])
+        # print(response["Body"].read().decode(encoding='UTF-8'))
+        json_data = json.load(response["Body"])
+        
+        # print(json_data)
+        # print(json_data["scenario_name"])
+        # print(json_data["user_string"])
+        name = json_data["scenario_name"]
+        user_string = "hwpc-user-inputs/" + json_data["user_string"]
+        # Uncomment this when we are going live with HWPC-Web
+        runCalculatorTask(name, user_string)
+        return 
+        
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
+    
+
+    
