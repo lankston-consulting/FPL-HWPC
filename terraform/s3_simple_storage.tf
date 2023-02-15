754e33175523cf20806bd6b3ba56c906b43a76b4
cg117634 <colton11gerth@gmail.com>
2023-02-15 13:01:50 -0600

    removed cloudtrail

--- terraform/s3_simple_storage.tf
+++ terraform/s3_simple_storage.tf
@@ -1,9 +1,3 @@
-resource "aws_s3_bucket" "aws_cloudtrail_logs_234659567514_0403b5d3" {
-  #   arn            = "aws:s3:::aws-cloudtrail-logs-234659567514-0403b5d3"
-  bucket = "aws-cloudtrail-logs-234659567514-0403b5d3"
-  #   hosted_zone_id = "Z3BJ6K6RIION7M"
-}
-
 # resource "aws_s3_bucket" "cf_templates_ui1evhwv02nm_us_west_2" {
 #   arn            = "arn:aws:s3:::cf-templates-ui1evhwv02nm-us-west-2"
 #   bucket         = "cf-templates-ui1evhwv02nm-us-west-2"
